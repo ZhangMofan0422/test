ec61e91940c665e95da89556bec1776b0ebfaac9
zhangmofan0422 <zhangmofan0422@gmail.com>
2025-01-11 10:23:14 +0800

    初始化

---
+++ C++/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "your name is : " << flush;
+    string name;
+    cin >> name;
+    cout << "Hello " << name << '!' << endl;
+    return 0;
+}
